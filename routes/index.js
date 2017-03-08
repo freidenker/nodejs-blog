6a0fad19e2e3bba5e75b513b7a23fdc9cbb33c1d
freidenker <elysianfieldslc@hotmail.com>
2017-03-09 00:21:37 +0800

    update at 2017 03-09 00:21 am

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 var crypto=require('crypto');
 User = require('../models/user.js');
 Post = require('../models/post.js');
+Comment = require('../models/comment.js');
 /*
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -188,6 +189,27 @@ router.get('/u/:name/:day/:title',function (req,res){
       error: req.flash('error').toString()
     });
   });
+});
+
+router.post('/u/:name/:day/:title', function(req,res){
+  var date=new Date(),
+      time=date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+(date.getMinutes()<10?'0'+date.getMinutes():date.getMinutes());
+      var comment = {
+        name: req.body.name,
+        email: req.body.email,
+        website: req.body.website,
+        time: time,
+        content: req.body.content
+      };
+      var newComment=new Comment(req.params.name, req.params.day, req.params.title, comment);
+      newComment.save(function(err){
+        if(err){
+          req.flash('error',err);
+          return res.redirect('back');
+        }
+        req.flash('success','留言成功!');
+        res.redirect('back');
+      });
 });
 
 router.get('/edit/:name/:day/:title',checkLogin);
