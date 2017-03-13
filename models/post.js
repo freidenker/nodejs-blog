6a42af540508b64e30f71a7f6ce60560e19c224b
winter <chao.lin2@hpe.com>
2017-03-13 19:07:30 +0800

    update at 2017-03-13 7:07pm with half repost

--- models/post.js
+++ models/post.js
@@ -28,6 +28,7 @@ Post.prototype.save=function(callback){
     tags: this.tags,
     post: this.post,
     comments: [],
+    reprint_info: {},
     pv: 0
   };
 
@@ -348,6 +349,77 @@ Post.search = function(keyword,callback){
           return callback(err);
         }
         callback(null,docs);
+      });
+    });
+  });
+};
+
+Post.reprint = function(reprint_from, reprint_to, callback){
+  mongodb.open(function(err,db){
+    if(err){
+      return callback(err);
+    }
+    db.collection('posts', function(err, collection){
+      if(err){
+        mongodb.close();
+        return callback(err);
+      }
+      collection.findOne({
+        "name": reprint_from.name,
+        "time.day": reprint_from.day,
+        "title": reprint_from.title
+      }, function(err, doc){
+        if(err){
+          mongodb.close();
+          return callback(err);
+        }
+
+        var date=new Date();
+        var time={
+          date: date,
+          year: date.getFullYear(),
+          month: date.getFullYear()+"-"+(date.getMonth()+1),
+          day: date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate(),
+          minute: date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+(date.getMinutes()<10?'0'+date.getMinutes():date.getMinutes())
+        }
+
+        delete doc._id;
+        doc.name=reprint_to.name;
+        doc.head=reprint_to.head;
+        doc.time=time;
+        doc.title=(doc.title.search(/[转载]/) > -1) ? doc.title : "[转载]"+doc.title;
+        doc.comments=[];
+        doc.reprint_info={"reprint_from": reprint_from};
+        doc.pv=0;
+
+        collection.update({
+          "name": reprint_from.name,
+          "time.day": reprint_from.day,
+          "title": reprint_from.title
+        },{
+          $push: {
+            "reprint_info.reprint_to":{
+              "name": doc.name,
+              "day": time.day,
+              "title": doc.title
+            }
+          }
+        }, function(err){
+          if(err){
+            mongodb.close();
+            return callback(err);
+          }
+        });
+        collection.insert(doc,{
+          safe: true
+        }, function(err, post){
+          mongodb.close();
+          if(err){
+            return callback(err);
+          }
+          callback(err, post[0]);
+        });
+
       });
     });
   });
--- routes/index.js
+++ routes/index.js
@@ -384,6 +384,28 @@ router.get('/remove/:name/:day/:title', function(req, res){
   });
 });
 
+router.get('/reprint/:name/:day/:title', checkLogin);
+router.get('/reprint/:name/:day/:title',function(req,res){
+  Post.edit(req.params.name, req.params.day, req.params.title, function(err, post){
+    if(err){
+      req.flash('error',err);
+      return res.redirect(back);
+    }
+    var currentUser=req.session.user,
+        reprint_from={name: post.name, day: post.time.day, title: post.title},
+        reprint_to={name: currentUser.name, head: currentUser.head};
+    Post.reprint(reprint_from,reprint_to,function(err,post){
+      if(err){
+        req.flash('error',err);
+        return res.redirect('back');
+      }
+      req.flash('success','转载成功!');
+      var url=encodeURI('/u/'+post.name+'/'+post.time.day+'/'+post.title);
+      res.redirect(url);
+    });
+  });
+});
+
 router.get('/post', checkLogin);
 router.get('/post',function(req,res){
 //  res.render('post',{title: '发表'});
